4ac832ad884d8e53cc5540203883884ed566a64d
benmosh21 <guybmontop@gmail.com>
2024-10-13 13:03:16 +0300

    Add button
    Add button class, and ADs button(cant buy yet(you can press but nothing will happen))

--- main.py
+++ main.py
@@ -28,18 +28,52 @@ class Text:
 
 
 class Button:
-    def __init__(self, text, color, pos, size):
-        self.pos = pos
-        self.size = size
-        self.color = color
-        self.text = text
+    def __init__(self, position, size, clr, cngclr, func, text, font, font_size, font_clr):
+        self.clr    = clr
+        self.size   = size
+        self.func   = func
+        self.surf   = pygame.Surface(size)
+        self.rect   = self.surf.get_rect(center=position)
+
+        if cngclr:
+            self.cngclr = cngclr
+        else:
+            self.cngclr = clr
+
+        if len(clr) == 4:
+            self.surf.set_alpha(clr[3])
+
+
+        self.font = pygame.font.SysFont(font, font_size)
+        self.txt = text
+        self.font_clr = font_clr
+        self.txt_surf = self.font.render(self.txt, 1, self.font_clr)
+        self.txt_rect = self.txt_surf.get_rect(center=[wh//2 for wh in self.size])
 
     def draw(self, screen):
-        self.button_surface = pygame.Surface(self.size)
-        self.text.pos = self.text.text_surface.get_rect(center=(self.button_surface.get_width()/2, self.button_surface.get_height()/2))
-    
-    def isClick(self):
-        pass
+        self.mouseover()
+
+        self.surf.fill(self.curclr)
+        self.surf.blit(self.txt_surf, self.txt_rect)
+        screen.blit(self.surf, self.rect)
+
+    def mouseover(self):
+        self.curclr = self.clr
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            self.curclr = self.cngclr
+
+    def call_back(self, *args):
+        if self.func:
+            return self.func(*args)
+        
+    def event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    if self.rect.collidepoint(pos):
+                        self.call_back()
+
 
 class Dimention:
     def __init__(self, name, baseCost, costMult, powerMult, amount):
@@ -48,6 +82,9 @@ class Dimention:
         self.costMult = costMult
         self.powerMult = powerMult
         self.amount = amount
+
+    def func(self):
+        print(self.name)
         
 
 class Antimatter:
@@ -59,11 +96,30 @@ class Antimatter:
                               Dimention("AD_3", pow(10, 4), pow(10, 5), 1.16, 0), Dimention("AD_4", pow(10, 6), pow(10, 6), 1.16, 0),
                               Dimention("AD_5", pow(10, 9), pow(10, 8), 1.16, 0), Dimention("AD_6", pow(10, 13), pow(10, 10), 1.23, 0),
                               Dimention("AD_7", pow(10, 18), pow(10, 12), 1.16, 0), Dimention("AD_8", pow(10, 24), pow(10, 15), 1.16, 0)]
-        
+        self.dButtons = []
+
+        self.DimentionsButton()
+
+
     def addAntimatter(self):
         for dimention in self.allDimentions:
             self.AntimatterAmount += dimention.amount*dimention.powerMult
         return self.AntimatterAmount
+    
+    def DimentionsButton(self):
+        count = 0
+        for dimention in self.allDimentions:
+            self.dButtons.append(Button((80, 150+60*count), (100,50), (200,200,200), (255, 0, 0), self.allDimentions[count].func, f"button {count+1}", "Roboto/Roboto=Black.ttf", 30,  (0,0,0)))
+            count += 1
+
+    def draw(self, screen):
+        for b in self.dButtons:
+            b.draw(screen)
+
+    def event(self, event):
+        for b in self.dButtons:
+            b.event(event)
+
 
 def numToExpones(num):
     num1 = num
@@ -73,6 +129,8 @@ def numToExpones(num):
     else:
         return str((f"{(num/(pow(10, base))):.2f}"))+"e"+str(base)
 
+def test():
+    print("test")
 
 def main(isRunning):
     score = 0
@@ -85,15 +143,21 @@ def main(isRunning):
         screen.fill("white")
 
         scoreText.draw(screen)
+        #button.draw(screen)
+
+        ADs.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isRunning = False
 
+            ADs.event(event)
+
         pygame.display.flip()
         scoreText.text = "Antimatter Amount " + str(numToExpones(ADs.AntimatterAmount))
         score+=ADs.addAntimatter()
-        #print(ADs.addAntimatter())
+
+
         clock.tick(50)
             
     pygame.quit()
--- test.py
+++ test.py
@@ -1,68 +1,107 @@
 import pygame
-import sys
 
-# Initialize Pygame
-pygame.init()
+class button:
+    def __init__(self, position, size, clr=[100, 100, 100], cngclr=None, func=None, text='', font="Segoe Print", font_size=16, font_clr=[0, 0, 0]):
+        self.clr    = clr
+        self.size   = size
+        self.func   = func
+        self.surf   = pygame.Surface(size)
+        self.rect   = self.surf.get_rect(center=position)
 
-clock=pygame.time.Clock()
+        if cngclr:
+            self.cngclr = cngclr
+        else:
+            self.cngclr = clr
 
-# Create a Pygame window
-window_size = (400, 400)
-screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption('Pygame Clickable Button')
-
-# Create a font object
-font = pygame.font.Font(None, 24)
-
-# Create a surface for the button
-button_surface = pygame.Surface((150, 50))
-
-# Render text on the button
-text = font.render("Click Me", True, (0, 0, 0))
-text_rect = text.get_rect(center=(button_surface.get_width()/2, button_surface.get_height()/2))
+        if len(clr) == 4:
+            self.surf.set_alpha(clr[3])
 
 
-# Create a pygame.Rect object that represents the button's boundaries
-button_rect = pygame.Rect(125, 125, 150, 50)  # Adjust the position as needed
+        self.font = pygame.font.SysFont(font, font_size)
+        self.txt = text
+        self.font_clr = font_clr
+        self.txt_surf = self.font.render(self.txt, 1, self.font_clr)
+        self.txt_rect = self.txt_surf.get_rect(center=[wh//2 for wh in self.size])
 
-# Start the main loop
-while True:
-    # Set the frame rate
-    clock.tick(60)
-    
-    # Fill the display with color
-    screen.fill((155, 255, 155))
+    def draw(self, screen):
+        self.mouseover()
 
-    # Get events from the event queue
-    for event in pygame.event.get():
-        # Check for the quit event
-        if event.type == pygame.QUIT:
-            # Quit the game
-            pygame.quit()
-            sys.exit()
+        self.surf.fill(self.curclr)
+        self.surf.blit(self.txt_surf, self.txt_rect)
+        screen.blit(self.surf, self.rect)
 
-     # Check for the mouse button down event
-    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        # Call the on_mouse_button_down() function
-        if button_rect.collidepoint(event.pos):
-            print("Button clicked!")
+    def mouseover(self):
+        self.curclr = self.clr
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            self.curclr = self.cngclr
 
-    # Check if the mouse is over the button. This will create the button hover effect
-    if button_rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
-    else:
-        pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
-        pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
-        pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
-        #pygame.draw.rect(button_surface, (0, 100, 0), (1, 48, 148, 10), 2)
-    
-    
-    
-    # Shwo the button text
-    button_surface.blit(text, text_rect)
+    def call_back(self, *args):
+        if self.func:
+            return self.func(*args)
 
-    # Draw the button on the screen
-    screen.blit(button_surface, (button_rect.x, button_rect.y))
+class text:
+    def __init__(self, msg, position, clr=[100, 100, 100], font="Segoe Print", font_size=15, mid=False):
+        self.position = position
+        self.font = pygame.font.SysFont(font, font_size)
+        self.txt_surf = self.font.render(msg, 1, clr)
 
-    # Update the game state
-    pygame.display.update()
+        if len(clr) == 4:
+            self.txt_surf.set_alpha(clr[3])
+
+        if mid:
+            self.position = self.txt_surf.get_rect(center=position)
+
+
+    def draw(self, screen):
+        screen.blit(self.txt_surf, self.position)
+
+
+
+
+# call back functions
+def fn1():
+    print('button1')
+def fn2():
+    print('button2')
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen_size = (300, 200)
+    size        = 10
+    clr         = [255, 0, 255]
+    bg          = (255, 255, 0)
+    font_size   = 15
+    font        = pygame.font.Font(None, font_size)
+    clock       = pygame.time.Clock()
+
+    screen    = pygame.display.set_mode(screen_size)
+    screen.fill(bg)
+
+    button1 = button(position=(80, 100), size=(100, 50), clr=(220, 220, 220), cngclr=(255, 0, 0), func=fn1, text='button1')
+    button2 = button((220, 100), (100, 50), (220, 220, 220), (255, 0, 0), fn2, 'button2')
+
+    button_list = [button1, button2]
+
+    crash = True
+    while crash:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crash = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    crash = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    for b in button_list:
+                        if b.rect.collidepoint(pos):
+                            b.call_back()
+
+        for b in button_list:
+            b.draw(screen)
+
+        pygame.display.update()
+        clock.tick(60)
